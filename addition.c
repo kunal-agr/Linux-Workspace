93b77e1c71d8755f4c802ec4d3c7419df48b7b98
Kunal_agr <kunalkagrawal2004@gmail.com>
2025-10-25 20:26:08 +0530

    first c code

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	//making boxes to store a integer variable
+	int a,b;
+
+	printf("Enter first number:");// Printing message for user so he can enter a number
+	scanf("%d",&a);				  // this function is used to read the input entered by user and store it in interge box a
+	
+	printf("\nEnter second number:");
+	scanf("%d",&b);
+	int c = a + b;	// creating another box c to store the addition of two integers
+	printf("\nTotal is :%d",c);	// here i am just printing the answer, we can print the value of a variable using printf
+	return 0;
+}
