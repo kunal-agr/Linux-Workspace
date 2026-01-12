30e1276603a164a1b9f812818544d15c7a62169b
kunal-agr <kunalkagrawal2004@gmail.com>
2026-01-12 18:19:48 +0530

    added age input and check in addition.c

--- addition.c
+++ addition.c
@@ -3,12 +3,15 @@ int main()
 {
 	//making boxes to store a integer variable
 	int a,b;
-	int roll;
+	int roll,age;
 
 	printf("\nEnter your roll number : "); // taking roll as credential
 	scanf("%d",&roll);
+	
+	printf("\nEnter your age:"); // taking age as a input
+	scanf("%d",&age);
 
-	if (roll > 0) // checking for valid roll numbers
+	if (roll > 0 && age > 3 age < 100) // checking for valid roll numbers and age
 	{
 
 			printf("Enter first number:");// Printing message for user so he can enter a number
@@ -30,6 +33,6 @@ int main()
 	}
 
 	else {
-		printf("\nEnter valid roll number !!"); // error message if someone enter wrong roll
+		printf("\nEnter valid roll number or age!!"); // error message if someone enters wrong roll or age
 	}
 }
