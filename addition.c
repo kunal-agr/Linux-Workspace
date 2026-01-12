72d438146a5cfcc54b56749eba6a434d14a1c098
kunal-agr <kunalkagrawal2004@gmail.com>
2026-01-12 17:53:39 +0530

    added roll number accepting check in addition.c

--- addition.c
+++ addition.c
@@ -3,21 +3,33 @@ int main()
 {
 	//making boxes to store a integer variable
 	int a,b;
+	int roll;
 
-	printf("Enter first number:");// Printing message for user so he can enter a number
-	scanf("%d",&a);				  // this function is used to read the input entered by user and store it in interge box a
-	
-	printf("\nEnter second number:");
-	scanf("%d",&b);
-	
-	int add = a + b;	// creating another box c to store the addition of two integers
-	int sub = a - b;
-	int mul = a * b;
-	int div = a / b;
+	printf("\nEnter your roll number : "); // taking roll as credential
+	scanf("%d",&roll);
 
-	printf("\nTotal is :%d",add);	// here i am just printing the answer, we can print the value of a variable using printf
-	printf("\nTotal is :%d",sub);
-	printf("\nTotal is :%d",mul);
-	printf("\nTotal is :%d",div);
-	return 0;
+	if (roll > 0) // checking for valid roll numbers
+	{
+
+			printf("Enter first number:");// Printing message for user so he can enter a number
+			scanf("%d",&a);				  // this function is used to read the input entered by user and store it in interge box a
+			
+			printf("\nEnter second number:");
+		scanf("%d",&b);
+		
+		int add = a + b;	// creating another box c to store the addition of two integers
+		int sub = a - b;
+		int mul = a * b;
+		int div = a / b;
+		
+		printf("\nTotal is :%d",add);	// here i am just printing the answer, we can print the value of a variable using printf
+		printf("\nTotal is :%d",sub);
+		printf("\nTotal is :%d",mul);
+		printf("\nTotal is :%d",div);
+		return 0;
+	}
+
+	else {
+		printf("\nEnter valid roll number !!"); // error message if someone enter wrong roll
+	}
 }
